34481d3e0846b3a7013d23bee03a272b9705e831
Illia <illiapikhotskyi@gmail.com>
2023-07-09 21:00:29 +0200

    images compression

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
+import webp from 'gulp-webp';
+
+gulp.task('convertAndMinify', async () => {
+	return await gulp
+		.src('./assets/*.{jpg,png}')
+		.pipe(imagemin())
+		.pipe(webp())
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('minifyImages', async () => {
+	return await gulp
+		.src('./assets/*.{jpg,png,webp}')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./dist'));
+}),
+	gulp.task('default', gulp.series('convertAndMinify', 'minifyImages'));
